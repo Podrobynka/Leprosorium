c0ac0921c4ed8b99000724bef34010336f4f1fd8
Podrobynka <yfgurda@gmail.com>
2018-10-16 01:33:04 +0300

    init database, make @db

--- app.rb
+++ app.rb
@@ -1,5 +1,15 @@
 require 'rubygems'
 require 'sinatra'
+require 'sqlite3'
+
+def init_db
+  @db = SQLite3::Database.new 'leprosorium.db'
+  @db.results_as_hash = true
+end
+
+before do
+  init_db
+end
 
 get '/' do
   erb :main
@@ -10,5 +20,6 @@ get '/new' do
 end
 
 post '/new' do
-  erb :new
+  content = params[:content]
+  erb "You typed #{content}"
 end
